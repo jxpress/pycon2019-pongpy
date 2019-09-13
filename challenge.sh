f3591c40b1450b7faf5f44195c537fd5f7d64fc1
Tatsuya Hirase <t.hirase@jxpress.net>
2019-09-13 10:15:52 +0900

    Add challenge script

---
+++ challenge.sh
@@ -0,0 +1,18 @@
+if [ "$1" == "" ]; then
+        echo "第一引数にGitHubアカウントを指定してください ex) `pistatium`"
+        exit 1
+fi
+
+echo PLAYER_NAME: $1
+
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+export PLAYER_NAME=$1
+
+wget -O challenger.py https://raw.githubusercontent.com/$PLAYER_NAME/pycon2019-pongpy/master/challenger.py
+
+if [ "$?" -ne "0" ]; then
+  echo "Sorry, cannot find your challenger.py"
+  exit 1
+fi
+
+pongpy challenger:ChallengerTeam enemy:EnemyTeam
