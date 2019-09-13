e5a54b823a58fa6294ecc5f30ea11e906b5a039f
Kimihiro N <kimihiro.nisimura@gmail.com>
2019-09-13 11:59:26 +0900

    :notebook: ドキュメント更新

--- enemy.py
+++ enemy.py
@@ -4,12 +4,20 @@ from pongpy.models.state import State
 
 
 class EnemyTeam(Team):
+    """
+    現在のボールの位置に合わせて動きます.
+
+    ※ このファイルはブースでの対戦時には利用されません.
+    """
+
     @property
     def name(self) -> str:
         return 'ENEMY'
 
     def atk_action(self, info: GameInfo, state: State) -> int:
-        return 1
+        direction = (state.ball_pos.y - state.mine_team.atk_pos.y) > 0
+        return info.atk_return_limit if direction else -info.atk_return_limit
 
     def def_action(self, info: GameInfo, state: State) -> int:
-        return 1
+        direction = (state.ball_pos.y - state.mine_team.def_pos.y) > 0
+        return info.def_return_limit if direction else -info.def_return_limit
